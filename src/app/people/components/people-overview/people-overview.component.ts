eb37b3cad879875e90c331ddcce37f02e5d9b118
maxlanger <max.langer92@googlemail.com>
2023-03-24 11:55:56 +0100

    mob next [ci-skip] [ci skip] [skip ci]
    lastFile:src/app/people/components/people-overview/people-overview.component.ts

--- src/app/people/components/people-overview/people-overview.component.ts
+++ src/app/people/components/people-overview/people-overview.component.ts
@@ -1,20 +1,34 @@
 import { Component, OnInit } from '@angular/core';
 import { PEOPLE_DATA } from './people-data';
 import People from '../../models/people';
+import { PeopleService } from '../../services/people.service';
 
 @Component({
   selector: 'app-people-overview',
   templateUrl: './people-overview.component.html',
   styleUrls: ['./people-overview.component.scss'],
 })
-export class PeopleOverviewComponent {
+export class PeopleOverviewComponent implements OnInit {
   filterValue: string = '';
   displayedColumns: string[] = ['name', 'gender', 'birth_year', 'homeworld'];
-  dataSource = PEOPLE_DATA;
+  dataSource: People[] = [];
+
+  constructor(private peopleService: PeopleService) {}
+
+  ngOnInit(): void {
+    this.peopleService.getPeople().subscribe((people) => {
+      this.dataSource = people.results;
+      console.log(people);
+    });
+  }
 
   getFilteredData(): People[] {
-    return this.dataSource.filter((person: People) => {
-      return person.name.toLowerCase().includes(this.filterValue.toLowerCase());
-    });
+    return this.dataSource
+      ? this.dataSource.filter((person: People) => {
+          return person.name
+            .toLowerCase()
+            .includes(this.filterValue.toLowerCase());
+        })
+      : [];
   }
 }
--- src/app/people/services/people.service.ts
+++ src/app/people/services/people.service.ts
@@ -1,12 +1,15 @@
 import { HttpClient } from '@angular/common/http';
+import { Injectable } from '@angular/core';
+import { Observable } from 'rxjs';
 import People from '../models/people';
 
+@Injectable({
+  providedIn: 'root',
+})
 export class PeopleService {
   constructor(private http: HttpClient) {}
 
-  getPeople(): Promise<People[] | undefined> {
-    return this.http
-      .get<People[] | undefined>('https://swapi.dev/api/people')
-      .toPromise();
+  getPeople(): Observable<People[]> {
+    return this.http.get<People[]>('https://swapi.dev/api/people');
   }
 }
