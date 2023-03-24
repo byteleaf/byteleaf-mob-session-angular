1ce9643a295fbddfd260d72de1dad18c9b323084
Simon Ittmann <sittmann@munichre.com>
2023-03-24 11:31:54 +0100

    mob next [ci-skip] [ci skip] [skip ci]
    lastFile:src/app/people/components/people-overview/people-overview.component.html

--- src/app/people/components/people-overview/people-overview.component.ts
+++ src/app/people/components/people-overview/people-overview.component.ts
@@ -1,5 +1,6 @@
-import { Component } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
 import { PEOPLE_DATA } from './people-data';
+import People from "../../models/people";
 
 @Component({
   selector: 'app-people-overview',
@@ -7,6 +8,15 @@ import { PEOPLE_DATA } from './people-data';
   styleUrls: ['./people-overview.component.scss'],
 })
 export class PeopleOverviewComponent {
+
+  filterValue: string = '';
   displayedColumns: string[] = ['name', 'gender', 'birth_year', 'homeworld'];
   dataSource = PEOPLE_DATA;
+
+  getData(): People[] {
+    return this.dataSource.filter((person: People) => {
+      return person.name.toLowerCase().includes(this.filterValue.toLowerCase());
+    });
+  }
+
 }
--- src/app/people/people.module.ts
+++ src/app/people/people.module.ts
@@ -2,9 +2,12 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { MatTableModule } from '@angular/material/table';
 import { PeopleOverviewComponent } from './components/people-overview/people-overview.component';
+import {MatFormFieldModule} from "@angular/material/form-field";
+import {MatInputModule} from "@angular/material/input";
+import {FormsModule} from "@angular/forms";
 
 @NgModule({
   declarations: [PeopleOverviewComponent],
-  imports: [CommonModule, MatTableModule],
+  imports: [CommonModule, MatTableModule, MatFormFieldModule, MatInputModule, FormsModule],
 })
 export class PeopleModule {}
